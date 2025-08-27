151e2eee12c505632d5fbc38ea9d4b054783b9cb
Dhruv <dhruv.sharma3403@gmail.com>
2025-08-27 15:39:17 +0530

    g

--- travelbooker/settings.py
+++ travelbooker/settings.py
@@ -23,9 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y+va=zr@$2(if(xivw_%lf7u6c!^y$m%zxw5gij-9f%tilzm#u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = ["rockinit2004.pythonanywhere.com", "127.0.0.1"]
 
 
 # Application definition
@@ -121,8 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
+
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
